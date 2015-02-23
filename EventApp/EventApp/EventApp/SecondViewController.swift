c225e7d486e4dc6e8b91329eb87f35812d8e0a07
Byrdann Fox <mobileupstate@gmail.com>
2015-02-23 16:26:23 -0500

    Random event apps for iOS...

---
+++ EventApp/EventApp/EventApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  EventApp
+//
+//  Created by Byrdann Fox on 2/23/15.
+//  Copyright (c) 2015 lycan.io. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
