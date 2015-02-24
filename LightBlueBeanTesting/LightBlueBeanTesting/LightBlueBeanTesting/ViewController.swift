a02da05bbd756611f162469c5f745dc84c7dfd73
Byrdann Fox <mobileupstate@gmail.com>
2015-02-24 00:42:58 -0500

    Updates...

---
+++ LightBlueBeanTesting/LightBlueBeanTesting/LightBlueBeanTesting/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  LightBlueBeanTesting
+//
+//  Created by Byrdann Fox on 2/23/15.
+//  Copyright (c) 2015 lycan.io. All rights reserved.
+//
+
+import UIKit
+import CoreData
+import EventKit
+import EventKitUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+        
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
